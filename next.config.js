00ab115143873081a096ed8914163ec261d2c92e
Tubagus <dutapanggulu@gmail.com>
2021-03-01 07:31:28 +0700

    memperbaiki kesalahan api prod

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ module.exports = {
     env: {
         APP_NAME: 'FAINMI',
         API_DEV: 'http://192.168.1.111:8080',
-        API_PROD: 'fainmi.herokuapp.com'
+        API_PROD: 'https://fainmi.herokuapp.com'
     },
     images: {
         domains: ['192.168.1.111', 'fainmi.vercel.app', 'fainmi.herokuapp.com']
