5b2675f9ef73b3d3d71c1a232a7ef5a7d3e1f6d2
Tubagus <dutapanggulu@gmail.com>
2021-03-09 02:55:14 +0700

    added debounce settings

--- src/lib/hooks/debounce.ts
+++ src/lib/hooks/debounce.ts
@@ -1,8 +1,14 @@
 import { useCallback } from 'react';
 import debounce from 'lodash.debounce';
 
-const useDebounce = (callback: any, delay: number) => {
-    return useCallback(debounce(callback, delay), [delay]);
+interface DebounceSettings {
+    leading?: boolean;
+    maxWait?: number;
+    trailing?: boolean;
+}
+
+const useDebounce = (callback: any, delay: number, settings?: DebounceSettings) => {
+    return useCallback(debounce(callback, delay, settings), [delay]);
 };
 
 export default useDebounce;
